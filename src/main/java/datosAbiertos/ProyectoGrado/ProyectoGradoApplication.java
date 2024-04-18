5fd0b75d41d37f6eb3527f6df80bce1f4ab65f73
juan_linares <linares.0@hotmail.com>
2024-04-18 10:13:19 -0500

    first commit

---
+++ src/main/java/datosAbiertos/ProyectoGrado/ProyectoGradoApplication.java
@@ -0,0 +1,13 @@
+package datosAbiertos.ProyectoGrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGradoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/datosAbiertos/ProyectoGrado/ProyectoGradoApplicationTests.java
@@ -0,0 +1,13 @@
+package datosAbiertos.ProyectoGrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
